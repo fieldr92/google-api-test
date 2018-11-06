df495d88f5e32e789cc3b4a1d23904bf80b1d0f9
fieldr <fieldr92@gmail.com>
2018-11-06 17:14:31 +0000

    Delete old index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const ejs = require('ejs');
 const home = require('./routes/home');
 const spreadsheet = require('./routes/spreadsheet');
+const signIn = require('./routes/signIn');
 
 // Express set up
 const app = express();
@@ -12,7 +13,8 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.use('/', home);
-app.use('/spreadsheet', spreadsheet)
+app.use('/spreadsheet', spreadsheet);
+app.use('/signIn', signIn);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
